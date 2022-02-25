2c6f9b9ecc9c1fe797215e1ef7c72bb8eea6f478
21akr <97340562+21akr@users.noreply.github.com>
2022-02-25 18:39:44 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+let a = 2;
+let b = 2;
+
+let calculation = prompt("calculation");
+if(calculation == "2+2") {
+  alert(`${a+b}`)
+}
+
+if(calculation == "2-2") {
+    alert(`${a-b}`)
+  }
+
+if(calculation == "2/2") {
+    alert(`${a/b}`)
+  }
+
+  if(calculation == "2*2") {
+    alert(`${a*b}`)
+  }
